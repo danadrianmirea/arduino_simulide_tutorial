b70a2f29f7e66df0f38e53d886688d3fd698ff67
Dan Adrian Mirea <damirea84@gmail.com>
2024-10-31 17:39:57 +0200

    del init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# arduino_simulide_tutorial" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/arduino_simulide_tutorial.git
-git push -u origin main
